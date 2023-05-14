624c2e2c0af59f06b87242d035c2b9b788818c8e
Gustavo <gusta.paz@hotmail.com>
2023-05-14 16:35:28 -0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
+import { logger } from '../common/middleware/logger.middleware';
+import { CatsModule } from '../cats/cats.module';
+import { CatsController } from '../cats/cats.controller';
+
+@Module({
+  imports: [CatsModule],
+})
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+  .apply(logger)
+  .forRoutes(CatsController);
+  }
+}
